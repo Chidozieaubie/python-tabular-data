1d1bfa6fc585b46b25bb58864d19bfec955414ba
chidozieaubie <cgu0001@auburn.edu>
2021-03-19 02:16:43 -0700

    Here I have a script that makes a scatter plot and plots a line of fit through the points using linear regression model.

---
+++ linear_regression.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python3
+# a script that plots scatterplot and makes a liner regression using the iris file as input
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+
+def get_regression_line(x, y):
+    """
+    This is a function that makes a scatter 
+    plot and linear regression 
+    parameters 
+    -----------
+    x : dataframe that would be used on x-axix
+    y : datframe to be used on y_axix
+
+    Returns
+    -------
+    plot and linear regression line fit. 
+    """
+    regression = stats.linregress(x, y)
+    slope = regression.slope
+    intercept = regression.intercept
+    plt.scatter(x, y, label = 'Data')
+    plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
+    plt.xlabel("Petal length (cm)")
+    plt.ylabel("Sepal length (cm)")
+    plt.legend()
+    return
+
+
+if __name__ == '__main__':
+
+    dataframe = pd.read_csv("iris.csv")
+    versicolor = dataframe[dataframe.species == "Iris_versicolor"]
+    virginica = dataframe[dataframe.species == "Iris_virginica"]
+    setosa = dataframe[dataframe.species == "Iris_setosa"]
+
+    x1 = versicolor.petal_length_cm
+    y1 = versicolor.sepal_length_cm
+    x2 = virginica.petal_length_cm
+    y2 = virginica.sepal_length_cm
+    x3 = setosa.petal_length_cm
+    y3 = setosa.sepal_length_cm
+
+    result1 = get_regression_line(x1, y1)
+    plt.savefig("versicolor_petal_v_sepal_length_regress.png")
+    plt.close()
+    result2 = get_regression_line(x2, y2)
+    plt.savefig("virginica_petal_v_sepal_length_regress.png")
+    plt.close()
+    result3 = get_regression_line(x3, y3)
+    plt.savefig("setosa_petal_v_sepal_length_regress.png")
+    plt.close('all')
+    quit()
